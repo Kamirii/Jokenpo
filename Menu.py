1d3766ea92df1586b56b7af684db49f37ed03c51
Kamirii <95884575+Kamirii@users.noreply.github.com>
2022-04-22 08:52:34 -0300

    Add files via upload
    menu iza

---
+++ Menu.py
@@ -0,0 +1,15 @@
+print('-=-' *20)
+print("Bem-vindo ao Jokenpô ")
+print(" Digite 1 para jogar com o computador")
+print(" Digite 2 para jogar com outro player")
+print(" Digite 0 para sair ")
+menu = int(input(' Digite um número:'))
+print('-=-' *20)
+if menu == 1:
+       print('oi')
+elif menu == 2:
+       print('tchau')
+elif menu == 0:
+       quit()
+
+
